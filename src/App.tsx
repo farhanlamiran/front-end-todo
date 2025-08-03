7e56dda43c536a7cf8268c83ef714f6ea85ab453
Farhan Lamiran <empirevirtual21@gmail.com>
2025-08-03 11:35:46 +0700

    revisi

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { Badge } from "./components/ui/badge" 
 
 const queryClient = new QueryClient();
 
@@ -15,17 +14,6 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <div className="p-6 space-y-4">
-          <Badge>Default</Badge>
-          <Badge variant="secondary"
-            className="cursor-pointer"
-            onClick={() => alert("Klik badge!")}
-            role="button"
-            tabIndex={0}>TEST</Badge>
-              <Badge variant="destructive">Dest</Badge>
-              <Badge variant="outline">Outline</Badge>
-              <Badge variant="default" className="bg-green-500">Custom Default</Badge>
-            </div>
         <Routes>
 
           <Route path="/" element={<Index />} />
